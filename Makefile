06e701303711221e5638d8076284ee15dab93677
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2015-12-06 01:39:23 +0100

    Control single channel using shift register

--- Makefile
+++ Makefile
@@ -2,15 +2,17 @@ program=hello
 
 hello: main.hex
 
-main.elf: main.cpp
-	avr-gcc -Os -std=c++1y -mmcu=atmega328p -o $@ $<
+CPPFLAGS=-Os -std=c++1y -mmcu=atmega328p -fno-exceptions
 
-main.hex: main.elf
+main.elf: main.cpp
+	avr-gcc ${CPPFLAGS} -o $@ $<
+
+%.hex: %.elf
 	avr-objcopy -R .eeprom -O ihex $< $@
 
-clean: main.hex main.elf
-	rm main.hex
-	rm main.elf
+clean:
+	rm *.hex
+	rm *.elf
 
 flash: main.hex
 	avrdude -u -c usbasp -p atmega328p -U flash:w:main.hex:i
--- main.cpp
+++ main.cpp
@@ -20,6 +20,68 @@ void blink() {
 	}
 }
 
+void srSetup() {
+	DDRC |= 0b00000111;
+	PORTC = 0b00000000;
+}
+
+void srClock() {
+	PORTC ^= 0b00000001;
+	PORTC ^= 0b00000001;
+}
+
+void srData(bool data) {
+	if(data) {
+		PORTC |= 0b00000010;
+	} else {
+		PORTC &= ~0b00000010;
+	}
+	_delay_ms(1);
+}
+
+void srLatch() {
+	PORTC ^= 0b00000100;
+	PORTC ^= 0b00000100;
+}
+
+void writeLatch(uint8_t data) {
+	for(int i = 0; i < 8; ++i) {
+		srData(data & (1 << i));
+		srClock();
+	}
+	
+	srLatch();
+}
+
+// R 0b11000000
+// G 0b00110000
+// B 0b00001100
+volatile uint8_t mt[4][8][8];
+
 int main (void) {
-	blink();
+	DDRC = 0b00000111;
+	
+	DDRD = 0b11111111;
+	PORTD = 0b11111111;
+	//blink();
+	
+	srSetup();
+	
+/*	uint8_t data = 0;
+	while(true) {
+		writeLatch(0b00000000);
+		_delay_ms(100);
+	}*/
+
+	while(true) {
+		for(int r = 0; r < 8; ++r) {
+			PORTD = 1 << r;
+			
+			for(int c = 0; c < 255; c++) {
+				writeLatch(~c);
+				_delay_ms(25);
+			}
+		}
+	
+	}
 }
