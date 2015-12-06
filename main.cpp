80e950552dc96509bb41be8b82347053f9e939dd
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2015-12-06 23:57:49 +0100

    Tune demo and add 16 shades of each color

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#define F_CPU 1000000UL
-#define F_OSC 1000000
+#define F_CPU 8000000UL
+#define F_OSC 8000000
 
 #include <string.h>
 #include <avr/io.h>
@@ -51,21 +51,28 @@ inline void writeLatch(uint8_t data) {
 	}
 }
 
-// R 0b11000000
-// G 0b00110000
-// B 0b00001100
-//volatile uint8_t mt[4][8][8];
+
+
+const uint8_t INTENSITIES = 16;
+const uint8_t MAX_INTENSITY = INTENSITIES - 1;
+
+const uint8_t ROWS = 8;
+const uint8_t MAX_ROW = ROWS - 1;
+
+const uint8_t COLLUMNS = 8;
+const uint8_t MAX_COLLUMN = COLLUMNS - 1;
 
 /**
  * Framebuffer
  * 
- * mt[ROW][COLUMN][COLOR] = INTENSITY (8bit)
+ * mt[COLOR_CNANNEL][INTENSITY_PLANE * 8 + row]
+ * 
+ * Framebuffer is cyclicaly rendered every time one row wit one intensity plane.
+ * Intensity is defined by number of intensity planes containing 1 in the place
+ * representing particular LED.
  */
-uint8_t mt[3][8][8];
+uint8_t mt[3][INTENSITIES * ROWS];
 
-const uint8_t MAX_INTENSITY = 7;
-const uint8_t MAX_ROW = 7;
-const uint8_t MAX_COLLUMN = 7;
 
 
 /**
@@ -76,32 +83,40 @@ const uint8_t MAX_COLLUMN = 7;
  */
 uint8_t row = 0;
 uint8_t intensity = 0;
+uint8_t ir = 0;
 SIGNAL(TIMER1_COMPA_vect) {
-	writeLatch(mt[0][intensity][row]);
-	writeLatch(mt[1][intensity][row]);
-	writeLatch(mt[2][intensity][row]);
+	writeLatch(mt[0][ir]);
+	writeLatch(mt[1][ir]);
+	writeLatch(mt[2][ir]);
 	
 	// Set current row
 	PORTD = 0;
 	srLatch();
-	PORTD = 1 << row;
-		
-	row++;
-	if(row > MAX_ROW) {
-		intensity++;
-		row = 0;
-	}
+	PORTD = 1 << ir % ROWS;
 	
-	if(intensity > MAX_INTENSITY) {
-		intensity = 0;
-	}
+	ir++;
+	//ir &= 0b00111111;
+	if(ir > (INTENSITIES * ROWS - 1))
+		ir = 0;
 }
 
 int main (void) {
-	DDRC = 0b00010111;
-	
+	// First disable output
+	PORTD = 0b00000000;
 	DDRD = 0b11111111;
-	PORTD = 0b11111111;
+	
+	
+	// 8Mhz - no prescaller system clock
+	asm volatile (
+	"st Z,%1" "\n\t"
+	"st Z,%2"
+	: :
+	"z" (&CLKPR),
+	"r" ((uint8_t) (1<<CLKPCE)),
+	"r" ((uint8_t) 0)  // new CLKPR value
+	);
+	
+	DDRC = 0b00010111;
 	
 	// Boot blink
 	PORTC |= 0b00010000;
@@ -117,7 +132,7 @@ int main (void) {
 	
 	
 	// Enable TIMER1 interrupt - plane switching
-	OCR1A  = 1; // Number to count up to
+	OCR1A  = 10; // Number to count up to
 	// COM1A1 COM1A0 COM1B1 COM1B0 – – WGM11 WGM10
 	TCCR1A = 0b0000'0101; // Clear Timer on Compare Match (CTC) mode
 	// ICNC1 ICES1 – WGM13 WGM12 CS12 CS11 CS10 
@@ -126,22 +141,46 @@ int main (void) {
 	TIMSK1 |= 1 << OCIE1A; // TC0 compare match A interrupt enable
 	sei();
 	
-	for(int i = 0 ; i <= MAX_INTENSITY; i ++) {
-		for(int r = 0 ; r <= MAX_ROW; r ++) {
-			mt[0][i][r] = 0b11111111;
-			mt[1][i][r] = 0b11111111;
-			mt[2][i][r] = 0b11111111;
+	uint8_t step = 0;
+	while(true) {
+		// Erase
+		for(int i = 0 ; i < INTENSITIES; i++) {
+			for(int r = 0 ; r < ROWS; r++) {
+				mt[0][i * ROWS + r] = 0b11111111;
+				mt[1][i * ROWS + r] = 0b11111111;
+				mt[2][i * ROWS + r] = 0b11111111;
+			}
 		}
-	}
-	
-	uint8_t ROW2INT[] = {0, 1, 2, 3, 4, 5, 6, 7};
-	
-	for(int r = 0; r < 8; ++r) {
-		const uint8_t intensity = ROW2INT[r];
-		for(int i = 0; i < intensity; ++i) {
-			mt[2][i][r] = ~(1 << r);
+		
+	/*	
+		// Diagonal
+		for(int r = 0; r < ROWS; ++r) {
+			const uint8_t intensity = (r + step) % INTENSITIES;
+			for(int i = 0; i < intensity; ++i) {
+				mt[2][i * ROWS + r] = ~(1 << r);
+			}
+		}*/
+		
+		
+		// Pattern
+		for(int i = 0 ; i <= MAX_INTENSITY; i++) {
+			for(int r = 0 ; r <= MAX_ROW; r++) {
+				for(int c = 0; c <= MAX_COLLUMN; c++) {
+					if(i <= (c + r*(step / 32) + step) % INTENSITIES) {
+						mt[0][i * ROWS + r] &= ~(1 << c);
+						mt[1][i * ROWS + r] &= ~(1 << c);
+						mt[2][i * ROWS + r] &= ~(1 << c);
+					}
+				}
+			}
 		}
+		
+		
+		
+		_delay_ms(50);
+		step++;
+		
+		// Liveness blink
+		PORTC ^= 0b00010000;
 	}
-
-	while(true);
 }
