5cd997b2727919217f085f5fa3a03ac21224e770
Vladimír Matěna <vlada.matena@gmail.com>
2016-02-09 23:40:12 +0100

    Refactor code

--- main.cpp
+++ main.cpp
@@ -70,116 +70,15 @@ uint16_t fb[ROWS][COLLUMNS]; // 0b----RRRRGGGGBBBB
 
 
 
-/**
- * Timer interrupt
- * 
- * Display output for this period of Timer
- * 
- */
-uint8_t row = 0;
-uint8_t intensity = 0;
-uint8_t ir = 0;
-inline void serviceScreen() {
-	writeLatch(mt[0][ir]);
-	writeLatch(mt[1][ir]);
-	writeLatch(mt[2][ir]);
-	
-	// Set current row
-	NEGPORT = 0;
-	srLatch();
-	NEGPORT = 1 << ir % ROWS;
-	
-	ir++;
-	if(ir > (INTENSITIES * ROWS - 1)) {
-		ir = 0;
-	}
-}
-SIGNAL(TIMER1_COMPA_vect) {
-	serviceScreen();
-}
-
-void uart_puts(const char *str);
-void uart_putchar(char c);
-char uart_getchar();
-
-bool state = false;
-uint8_t pos = 0;
-uint16_t data = 0;
-
-void render();
-bool receiving = false;
-
-
-bool dirty = false;
-// Serial coomunication interrupt
-ISR(USART_RX_vect) {
-//	receiving = true;
-	/*
-	while(RXC0) {
-		uint8_t c = UDR0;
-		((uint8_t*)fb)[pos++ % 128] = c;
-		
-		if(pos % 128 == 0) {
-			receiving = false;
-			render();
-		}
-	}
-*/
-	uint8_t c = UDR0;
-	uint8_t d = uart_getchar();
-	//loop_until_bit_is_set(UCSR0A, UDRE0);
-	//UDR0 = c;
-	/*
-	if(!state) {
-		data = c;
-		state = true;
-		return;
-	} else {
-		data = (data << 8) | c;
-		state = false;
-	}*/
-	
-	data = d;
-	data = data << 8;
-	data = data | c;
-	
-	// reset - new image
-	if(data == 0b1000000000000000) {
-		pos = 0;
-//		receiving = true;
-		return;
-	}
-	
-	// render - end of image
-	if(data == 0b0100000000000000) {
-//		receiving = false;
-		dirty = true;
-		return;
-	}
-	
-	//fb[pos / 8][pos % 8] = data;
-	//pos++;
-	
-	((uint16_t*)fb)[pos++ % 64] = data;
-	
-/*	
-	((uint8_t*)fb)[pos++ % 128] = c;
-	
-	if(pos % 128 == 0) {
-		dirty = true;
-	}*/
-
-}
-
 void uart_init(void) {
 	UBRR0H = UBRRH_VALUE;
 	UBRR0L = UBRRL_VALUE;
-/*
+
 #if USE_2X
     UCSR0A |= _BV(U2X0);
 #else
     UCSR0A &= ~(_BV(U2X0));
-#endif*/
+#endif
 
 	UCSR0B = _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0); // Enable RX and TX and interrupt
 	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); // 8-bit data
@@ -195,10 +94,6 @@ void uart_puts(const char *str) {
 		uart_putchar(*str++);
 	}
 }
-/*void uart_putchar(char c) {
-    UDR0 = c;
-    loop_until_bit_is_set(UCSR0A, TXC0); // Wait until transmission ready. 
-}*/
 
 char uart_getchar(void) {
     loop_until_bit_is_set(UCSR0A, RXC0); /* Wait until data exists. */
@@ -239,14 +134,54 @@ void render() {
 	}
 }
 
-int main (void) {
-	// First disable output
-	NEGPORT = 0b00000000;
-	NEGDDR = 0b11111111;
+uint8_t pos = 0;
+bool dirty = false;
+// Serial coomunication interrupt
+ISR(USART_RX_vect) {
+	union {
+		uint16_t val;
+		uint8_t bytes[2];
+	} data;
 	
-	_delay_ms(100);
+	data.bytes[0] = UDR0;
+	data.bytes[1] = uart_getchar();
 	
+	// reset - new image
+	if(data.val == 0b1000000000000000) {
+		pos = 0;
+		return;
+	}
 	
+	// render - end of image
+	if(data.val == 0b0100000000000000) {
+		dirty = true;
+		return;
+	}
+	
+	((uint16_t*)fb)[pos++ % 64] = data.val;
+}
+
+/**
+ * Display output for this period of Timer
+ */
+uint8_t ir = 0;
+inline void serviceScreen() {
+	writeLatch(mt[0][ir]);
+	writeLatch(mt[1][ir]);
+	writeLatch(mt[2][ir]);
+	
+	// Set current row
+	NEGPORT = 0;
+	srLatch();
+	NEGPORT = 1 << ir % ROWS;
+	
+	ir++;
+	if(ir > (INTENSITIES * ROWS - 1)) {
+		ir = 0;
+	}
+}
+
+void clockTo8MHz() {
 	// 8Mhz - no prescaller system clock
 	asm volatile (
 	"st Z,%1" "\n\t"
@@ -256,6 +191,17 @@ int main (void) {
 	"r" ((uint8_t) (1<<CLKPCE)),
 	"r" ((uint8_t) 0)  // new CLKPR value
 	);
+}
+
+
+int main (void) {
+	// First disable output
+	NEGPORT = 0b00000000;
+	NEGDDR = 0b11111111;
+	
+	_delay_ms(100);
+	
+	clockTo8MHz();
 	
 	DDRC = 0b00010111;
 	
@@ -265,17 +211,6 @@ int main (void) {
 	writeLatch(0b11111111);
 	writeLatch(0b11111111);
 	writeLatch(0b11111111);
-	
-	
-/*	// Enable TIMER1 interrupt - plane switching
-	OCR1A  = 10; // Number to count up to
-	// COM1A1 COM1A0 COM1B1 COM1B0 – – WGM11 WGM10
-	TCCR1A = 0b0000'0101; // Clear Timer on Compare Match (CTC) mode
-	// ICNC1 ICES1 – WGM13 WGM12 CS12 CS11 CS10 
-	TCCR1B = 0b0001'0011; // clock source CLK/1024, start timer
-	TIFR1 |= 1 << 6; // clear interrupt flag
-	TIMSK1 |= 1 << OCIE1A; // TC0 compare match A interrupt enable*/
-	sei();
 	
 	// Demo pattern
 	for(int r = 0; r < 8; r++) {
@@ -287,15 +222,10 @@ int main (void) {
 	
 	
 	uart_init();
-	
-	
-//	uart_puts("Waiting for input image\n\r");
+	sei();
 	
 	while(true) {
-//		_delay_ms(10);
-		if(!receiving) {
-			serviceScreen();
-		}
+		serviceScreen();
 		
 		if(dirty) {
 			render();
